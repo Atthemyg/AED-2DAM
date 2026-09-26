78d3271d02629d6ac1d3d948fc2073672cbfb0d4
Atteneri <atthemyg@gmail.com>
2026-09-26 16:21:00 +0100

    Hago OrdenacionServiceImpl

--- Repaso/repaso-por-bloques-java/src/main/java/com/docencia/algoritmos/OrdenacionServiceImpl.java
+++ Repaso/repaso-por-bloques-java/src/main/java/com/docencia/algoritmos/OrdenacionServiceImpl.java
@@ -8,32 +8,75 @@ public class OrdenacionServiceImpl implements OrdenacionService {
 
     @Override
     public List<Integer> ordenarBurbujaAscendente(List<Integer> numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ordenarBurbujaAscendente'");
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        // [5, 2, 8, 1]
+        for (int i = 0; i < numeros.size() - 1; i++) {
+            for (int j = 0; j < numeros.size() - 1 - i; j++) {
+                if (numeros.get(j) > numeros.get(j + 1)) {
+                    Integer temporal = numeros.get(j);
+                    numeros.set(j, numeros.get(j + 1));
+                    numeros.set(j + 1, temporal);
+                }
+            }
+        }
+        return numeros;
     }
 
     @Override
     public List<Integer> ordenarBurbujaDescendente(List<Integer> numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ordenarBurbujaDescendente'");
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < numeros.size() - 1; i++) {
+            for (int j = 0; j < numeros.size() - 1 - i; j++) {
+                if (numeros.get(j) < numeros.get(j + 1)) {
+                    Integer temporal = numeros.get(j);
+                    numeros.set(j, numeros.get(j + 1));
+                    numeros.set(j + 1, temporal);
+                }
+            }
+        }
+        return numeros;
     }
 
     @Override
     public List<String> ordenarPalabrasAlfabeticamente(List<String> palabras) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ordenarPalabrasAlfabeticamente'");
+        if (palabras == null || palabras.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        palabras.sort(null);
+
+        return palabras;
     }
 
     @Override
     public List<Integer> invertirLista(List<Integer> numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'invertirLista'");
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> resultado = new ArrayList<>();
+
+        for (int i = numeros.size() - 1; i >= 0; i--) {
+            resultado.add(numeros.get(i));
+        }
+
+        return resultado;
     }
 
     @Override
     public Boolean estaOrdenadaAscendente(List<Integer> numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'estaOrdenadaAscendente'");
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < numeros.size() - 1; i++) {
+            if (numeros.get(i) > numeros.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
     
 }
